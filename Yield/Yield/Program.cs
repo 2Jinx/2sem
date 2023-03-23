56f63c1277c75db5392cf2379739948864a60703
Jinx <DVStanchu@stud.kpfu.ru>
2023-03-23 13:29:24 +0300

    IEnumearble

---
+++ Yield/Yield/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.ComponentModel;
+using Yield;
+
+internal class Program
+{
+    private static void Main()
+    {
+        try
+        {
+            var text = "are how Hi you?";
+            WordsInText s = new WordsInText(text);
+            foreach (var item in s)
+                Console.WriteLine(item);
+            Console.WriteLine();
+
+            var text1 = "1 20 300 4000 50000 600000";
+            WordsInText s1 = new WordsInText(text1);
+            foreach (var c in s1)
+                Console.WriteLine(c);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+}
+
---
+++ Yield/Yield/WordsInText.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yield;
+
+namespace Yield
+{
+    public class WordsInText : IEnumerable<string>
+    {
+        public string[] words;
+        public SortEnumerator sortwords;
+        public WordsInText(string text)
+        {
+            words = text.Split(' ');
+            sortwords = new SortEnumerator(words);
+            sortwords.GetSorted();
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return new MyClass(words);
+
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
