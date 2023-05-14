036c2388d018f46d7567f9da137c8980b1f4f45b
2Jinx <113981832+2Jinx@users.noreply.github.com>
2023-05-14 23:02:10 +0300

    B-TreeApp

--- B-Tree/B-Tree/Program.cs
+++ B-Tree/B-Tree/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Text;
 
 using B_Tree;
@@ -7,17 +8,50 @@ public class Program
 {
     public static void Main()
     {
-        BTree<int> tree = new BTree<int>(5);
-        tree.Add(1);
-        tree.Add(2);
-        tree.Add(3);
-        tree.Add(4);
-        tree.Add(5);
-        tree.Add(6);
-        Console.WriteLine(tree.Get(2));
-        Console.WriteLine(tree);
-        tree.Delete(2);
-        Console.WriteLine(tree);
+        try
+        {
+            GetTime();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public static void GetTime()
+    {
+        BTree<int> tree = new BTree<int>(10000);
+        Random r = new Random();
+        Stopwatch watch = new Stopwatch();
+        int k = 0;
+        Console.WriteLine("Время добавления элементов в структуру\n");
+        for (int i = 0; i < 10000; i++)
+        {
+            watch.Start();
+            tree.Add(r.Next(-100, 100));
+            Console.Write($"{watch.ElapsedMilliseconds} ");
+        }
+        watch.Stop();
+        watch.Reset();
+        Console.WriteLine("\nВремя удаления элементов из структуры\n");
+        for (int i = 0; i < 1000; i++)
+        {
+            watch.Start();
+            tree.Delete(k);
+            Console.Write($"{watch.ElapsedMilliseconds} ");
+            k += 5;
+        }
+        watch.Stop();
+        watch.Reset();
+        k = 0;
+        Console.WriteLine("\nВремя получения элементов из структуры\n");
+        for (int i = 0; i < 100; i++)
+        {
+            watch.Start();
+            tree.Get(k);
+            Console.Write($"{watch.ElapsedMilliseconds} ");
+            k += 10;
+        }
     }
 }
 
